e48d5124d13f33174121471029c201411bb151c8
Sergei_Levitin <Welxeor05@gmail.com>
2023-05-28 18:40:21 +0300

    add dz4

---
+++ dz4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите первое число ");
+ int numberA =  int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+ int numberB =  int.Parse(Console.ReadLine());
+ Console.Write("Введите третье число ");
+ int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+ if (numberA > max) max = numberA;
+ if (numberB > max) max = numberB;
+ if (numberC > max) max = numberC;
+
+ Console.Write("Максимальное число = ");
+ Console.WriteLine(max);
+
